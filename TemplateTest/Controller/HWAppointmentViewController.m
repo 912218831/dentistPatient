4834c6c587c3fb0f3034d8dfd7dd9a9bb0424d36
yangqinglong <912218831@qq.com>
2017-09-13 15:01:00 +0800

    我的预约列表

--- TemplateTest/Controller/HWAppointmentViewController.m
+++ TemplateTest/Controller/HWAppointmentViewController.m
@@ -8,8 +8,8 @@
 
 #import "HWAppointmentViewController.h"
 
-@interface HWAppointmentViewController ()
-
+@interface HWAppointmentViewController ()<UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout>
+@property(strong,nonatomic)UICollectionView * collectionView;
 @end
 
 @implementation HWAppointmentViewController
@@ -17,21 +17,66 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.titleView = [Utility navTitleView:@"预约"];
+    [self.view addSubview:self.collectionView];
 }
+
+
+- (UICollectionView *)collectionView
+{
+    if (_collectionView == nil) {
+        UICollectionViewFlowLayout * flowLayout = [[UICollectionViewFlowLayout alloc] init];
+        flowLayout.scrollDirection = UICollectionViewScrollDirectionVertical;
+        _collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, CONTENT_HEIGHT - 49) collectionViewLayout:flowLayout];
+        _collectionView.dataSource = self;
+        _collectionView.delegate = self;
+        _collectionView.backgroundColor = COLOR_F0F0F0;
+        [_collectionView registerNib:[UINib nibWithNibName:@"HWAppointmentCell" bundle:nil] forCellWithReuseIdentifier:@"HWAppointmentCell"];
+    }
+    return _collectionView;
+}
+
+#pragma UICollectionView datasource && delegate
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake((kScreenWidth-40)/2, 220);
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section
+{
+    return 10;
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section
+{
+    return 10;
+}
+
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section
+{
+    return UIEdgeInsetsMake(10, 15, 15, 10);
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return [collectionView dequeueReusableCellWithReuseIdentifier:@"HWAppointmentCell" forIndexPath:indexPath];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
---
+++ TemplateTest/View/Cell/HWAppointmentCell.m
@@ -0,0 +1,18 @@
+//
+//  HWAppointmentCell.m
+//  TemplateTest
+//
+//  Created by 杨庆龙 on 2017/9/13.
+//  Copyright © 2017年 caijingpeng.haowu. All rights reserved.
+//
+
+#import "HWAppointmentCell.h"
+
+@implementation HWAppointmentCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+
+}
+
+@end
